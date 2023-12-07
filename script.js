eb6c03490b58eaddcab3e9af440dae4dd75dac6d
jakerusa <jakerusalovims@gmail.com>
2023-12-07 12:31:19 -0500

    delete script 2 adding more names

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const names = [
-    "Alice", "Bob", "Charlie", "David", "Emma", "Frank", "Grace", "Henry", "Ivy", "Jack"
+    "Alice", "Bob", "Charlie", "David", "Emma", "Frank", "Grace", "Henry", "Ivy", "Jack", "Liam", "Olivia", "Noah", "Emma", "Sophia", "Jackson", "Ava", "Lucas", "Isabella", "Oliver",
+    "Amelia", "Benjamin", "Mia", "Elijah", "Charlotte", "Henry", "Caden", "Aria", "Ethan", "Scarlett",
+    "Alexander", "Luna", "Sebastian", "Evelyn", "James", "Abigail", "Carter", "Chloe", "Mateo", "Grace",
+    "Samuel", "Harper", "Daniel", "Emily", "Michael", "Lily", "Logan", "Layla", "Leo", "Avery", "Nathan",
+    "Sofia", "David", "Ella", "Joseph", "Riley", "Isaac", "Aubrey", "Lincoln", "Camila"
 ];
 
 function generateName() {
