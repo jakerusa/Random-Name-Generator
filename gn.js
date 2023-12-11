76c9679cbd9ec465a007e115d3ff08c24fb01ab2
jakerusa <jakerusalovims@gmail.com>
2023-12-10 19:25:52 -0500

    add gn file

---
+++ gn.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+// Specify the path to your CSV file
+const filePath = './names.csv';
+let names;
+
+// Read the CSV file
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading the file:', err);
+        return;
+    }
+
+    // Split the content into lines
+    const lines = data.split('\n');
+
+    // Extract the fourth column from each line
+    const fourthColumnList = lines.map(line => {
+        const columns = line.trim().split('\t');
+        return columns[3]; // Index 3 corresponds to the fourth column
+    });
+
+    // Output the list of fourth column values
+    console.log(fourthColumnList);
+    names = fourthColumnList;
+
+    // Additional functionality, if needed
+    generateName();
+});
+
+function generateName() {
+    const randomIndex = Math.floor(Math.random() * names.length);
+    const generatedName = names[randomIndex];
+    document.getElementById('generatedName').textContent = generatedName;
+}
